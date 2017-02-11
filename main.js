ceae01a53158895535df205c882bd24841db84e8
C Wilkosz <theewilkyway@gmail.com>
2017-02-11 11:14:25 -0600

    outLined

---
+++ main.js
@@ -0,0 +1,40 @@
+
+ var button = document.getElementById("How Tall");
+ console.log("button", button);
+var radio = document.getElementsByName("Floor space wanted");
+ console.log("radio", radio);
+
+
+ var = //for colums
+
+ var =//for rows
+
+
+
+
+
+for (int i = 0; i < 4; i++)
+	{ 
+   for (int j = i; j < 4; j++)
+	{
+		System.out.print(" ");
+ }
+for (int k = 0; k <= i; k++) {
+	("* ")
+
+
+
+
+//1. A key that specifies the height of the pine tree.
+//1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
+//1. A key that specifies which character to use to build the pine tree.
+//1. The character to use should be from user input in a `<input type="text">` field in the DOM.
+// for (var i =0; i<alphabet.length; i++){
+//     stackLetters += alphabet[i]
+//   	console.log( stackLetters) ;
+//   	if ((i + 1) % 3 === 0){
+//   		stackLetters +=" ";
+
+//   	}
+
+// }
