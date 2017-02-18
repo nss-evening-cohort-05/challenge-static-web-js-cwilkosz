7e21c8724bb550f265b32ea31dc581e1df2a7d07
C Wilkosz <theewilkyway@gmail.com>
2017-02-18 12:06:32 -0600

    work

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 
- var button = document.getElementById("How Tall");
- console.log("button", button);
-var radio = document.getElementsByName("Floor space wanted");
- console.log("radio", radio);
+// 
+
+function growTree (inputInfo) {
+  //value from how tall
+  var treeHeight = document.getElementById("treeHeight").value;
+  //value from character
+  var treeChar = document.getElementById("treeChar").value;
+  //add space
+  var growTree = "";
+
+  
+     {for (var i =0; +<treeHeight.height; i ++)}
+     
+     {for (var j = 0; +<treeHeight.height - (i + 1); j++) 
+         growTree +=" ";}
+  
+     for (var t = 0; t ((i * 2)+ 1); t++)
+      growTree +="_";
+    
+   
+console.log(growTree)
+button.addEventListener("click", growTree);
+
+}
 
 
- var = //for colums
-
- var =//for rows
 
 
 
 
 
-for (int i = 0; i < 4; i++)
-	{ 
-   for (int j = i; j < 4; j++)
-	{
-		System.out.print(" ");
- }
-for (int k = 0; k <= i; k++) {
-	("* ")
+// A key that specifies the height of the pine tree.
+// The value for the height of the tree should be from user input in a <input type="text"> field in the DOM.
+// A key that specifies which character to use to build the pine tree.
+// The character to use should be from user input in a <input type="text"> field in the DOM.
+// Once the user enters in a number, and a character, the user can either then just press the enter key (as long as the cursor is in one of the input fields), or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
 
+// If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value.
+
+// Grow your tree
 
 
 
@@ -29,6 +46,7 @@ for (int k = 0; k <= i; k++) {
 //1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
 //1. A key that specifies which character to use to build the pine tree.
 //1. The character to use should be from user input in a `<input type="text">` field in the DOM.
+
 // for (var i =0; i<alphabet.length; i++){
 //     stackLetters += alphabet[i]
 //   	console.log( stackLetters) ;
@@ -38,3 +56,4 @@ for (int k = 0; k <= i; k++) {
 //   	}
 
 // }
+//button.addEventListener("click");
